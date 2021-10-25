c9a57499f064a7042991c543983a17532d19e4d4
Pedro Bento <fc52823@alunos.fc.ul.pt>
2021-10-25 20:40:37 +0100

    Assets à toa e dash

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool dash = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if(Input.GetButtonDown("Dash"))
+        {
+            dash = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump, dash);
+        jump = false;
+        dash = false;
+    }
+}
